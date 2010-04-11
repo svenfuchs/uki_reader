bf79b70592aaf540955a013098591d5d11f2f730
Sven Fuchs <svenfuchs@artweb-design.de>
2010-04-11 15:03:02 +0200

    add readme

--- hook.js
+++ hook.js
@@ -1,4 +1,5 @@
 var node = document.createElement('script');
 node.type = 'text/javascript';
-node.src = 'http://localhost:21119/uki_reader.cjs';
+// node.src = 'http://localhost:21119/uki_reader.cjs';
+node.src = 'http://github.com/svenfuchs/uki_reader/raw/master/build/uki_reader.js';
 document.getElementsByTagName('body')[0].appendChild(node);
